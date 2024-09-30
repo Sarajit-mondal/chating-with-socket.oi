7f2653c64071db493b64acc12cfaf2b856af05e2
Sarajit-mondal <bdsarajit499@gmail.com>
2024-09-30 17:55:40 +0600

    full setem complete

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./backend/data/data");
+dotenv.config();
 const app = express();
 const port = 3000;
 
@@ -6,6 +9,10 @@ app.get("/", (req, res) => {
   res.send("server is running......");
 });
 
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
 app.listen(port, () => {
-  console.log(`http://localhost: ${port}`);
+  console.log(`http://localhost:${port}`);
 });
