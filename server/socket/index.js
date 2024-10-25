88da1dd85f9ac81a207fa737878940ddc2532aac
sarajit <bdsarajit499@gamil.com>
2024-10-25 22:12:45 +0600

    add some thing

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,14 +1,10 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
-<<<<<<< HEAD
 const {
   ConversationModel,
   MessageModel,
 } = require("../models/ConversationModel");
-=======
-const { ConversationModel, MessageModel } = require("../models/ConversationModel");
->>>>>>> 2c737f076d4e671d651ca01526df00920b05d932
 const app = express();
 
 // socket connection
@@ -26,111 +22,6 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
   console.log("A user connected:", socket.id);
   // const user = socket.handshake.auth.user;
-<<<<<<< HEAD
-
-  // convarsection
-  // socket.on("new message", async (data) => {
-  //   // check conversation is available both user
-  //   let conversation = await ConversationModel.findOne({
-  //     $or: [
-  //       { sender: data?.sender, reciver: data?.reciver },
-  //       { sender: data?.reciver, reciver: data?.sender },
-  //     ],
-  //   });
-  //   // conversation is not available
-  //   if (!conversation) {
-  //     const createConversation = await ConversationModel({
-  //       sender: data?.sender,
-  //       reciver: data.reciver,
-  //     });
-  //     conversation = await createConversation.save();
-  //   }
-  //   const message = new MessageModel({
-  //     text: data?.text,
-  //     imageUrl: data.imageUrl,
-  //     videoUrl: data?.videoUrl,
-  //     msgByUserId: data?.msgByUserId,
-  //   });
-  //   const saveMessage = await message.save();
-
-  //   const updateConversation = await ConversationModel.updateOne(
-  //     { _id: conversation._id },
-  //     {
-  //       $push: { messages: saveMessage?._id },
-  //     }
-  //   );
-
-  //   const getConversationMessage = await ConversationModel.findOne({
-  //     $or: [
-  //       { sender: data?.sender, reciver: data?.reciver },
-  //       { sender: data?.reciver, reciver: data?.sender },
-  //     ],
-  //   })
-  //     .populate("messages")
-  //     .sort({ updatedAt: -1 });
-
-  //   io.to(data?.reciver).emit("sendMessage", getConversationMessage);
-  //   // io.to(data?.reciver).emit("message", getConversationMessage);
-
-  //   console.log(getConversationMessage);
-  // });
-  // Listen for a new message event
-  socket.on("new message", async (data) => {
-    // Find if conversation exists between sender and receiver
-    let conversation = await ConversationModel.findOne({
-      $or: [
-        { sender: data?.sender, reciver: data?.reciver },
-        { sender: data?.reciver, reciver: data?.sender },
-      ],
-    });
-
-    // If conversation doesn't exist, create one
-    if (!conversation) {
-      const createConversation = new ConversationModel({
-        sender: data?.sender,
-        reciver: data?.reciver,
-      });
-      conversation = await createConversation.save();
-    }
-
-    // Save the message to the database
-    const message = new MessageModel({
-      text: data?.text,
-      imageUrl: data?.imageUrl,
-      videoUrl: data?.videoUrl,
-      msgByUserId: data?.msgByUserId,
-    });
-    const savedMessage = await message.save();
-
-    // Update conversation with the new message
-    await ConversationModel.updateOne(
-      { _id: conversation._id },
-      {
-        $push: { messages: savedMessage?._id },
-      }
-    );
-    // // reciver message message
-    // await ConversationModel.updateOne(
-    //   { _id: conversation._id },
-    //   {
-    //     $push: { messages: savedMessage?._id },
-    //   }
-    // );
-
-    // Fetch updated conversation with populated messages
-    const updatedConversation = await ConversationModel.findOne({
-      $or: [
-        { sender: data?.sender, reciver: data?.reciver },
-        { sender: data?.reciver, reciver: data?.sender },
-      ],
-    })
-      .populate("messages")
-      .sort({ updatedAt: -1 });
-
-    // Emit updated conversation data to both sender and receiver
-    io.emit("getMessage", updatedConversation);
-  });
-=======
 
 
   // convarsection
@@ -178,7 +69,6 @@ io.on("connection", (socket) => {
   })
   // convarsection
 
->>>>>>> 2c737f076d4e671d651ca01526df00920b05d932
 
   // convarsection
 
